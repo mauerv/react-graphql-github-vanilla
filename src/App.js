417c4454c279c16abc45a9c3fc22a662fc9c8946
Mauro <mauro@osn.global>
2020-06-20 17:57:42 -0300

    Add initial form to fetch info about a repo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+  },
+});
 
 const TITLE = "React GraphQL GitHub Client";
 
 class App extends Component {
+  state = {
+    path: "the-road-to-learn-react/the-road-to-learn-react",
+  };
+
+  componentDidMount() {}
+
+  onChange = (event) => {
+    this.setState({
+      path: event.target.value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   render() {
+    const { path } = this.state;
     return (
       <div>
         <h1>{TITLE}</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">Show open issues for https://github.com/</label>
+          <input
+            type="text"
+            id="url"
+            onChange={this.onChange}
+            value={path}
+            style={{ width: "300px" }}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <hr />
       </div>
     );
   }
